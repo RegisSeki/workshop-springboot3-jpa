cae10ed2b810a19b500b71b35d4c185ccf2358bc
RegisSeki <regisseki@gmail.com>
2022-10-25 16:37:49 -0300

    Create Project, User entity and resource, H2 config

---
+++ src/main/java/com/rtseki/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.rtseki.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rtseki.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Regis", "regis@email.com", "991848854", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+}
