ff1bb3422678b5fd0130752fae3b6a61f971f208
RegisSeki <regisseki@gmail.com>
2022-10-27 08:41:45 -0300

    Payment, one-to-one association

--- src/main/java/com/rtseki/course/config/TestConfig.java
+++ src/main/java/com/rtseki/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rtseki.course.entities.Category;
 import com.rtseki.course.entities.Order;
 import com.rtseki.course.entities.OrderItem;
+import com.rtseki.course.entities.Payment;
 import com.rtseki.course.entities.Product;
 import com.rtseki.course.entities.User;
 import com.rtseki.course.entities.enums.OrderStatus;
@@ -32,7 +33,6 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
-	
 	
 	@Autowired
 	private ProductRepository productRepository;
@@ -81,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
