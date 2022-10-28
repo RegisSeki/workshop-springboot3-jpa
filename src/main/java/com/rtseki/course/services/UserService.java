aaa37f0a63848af5d605534b23a14f43e8fb6b27
RegisSeki <regisseki@gmail.com>
2022-10-28 08:50:49 -0300

    Exception Handler at User findById

--- src/main/java/com/rtseki/course/services/UserService.java
+++ src/main/java/com/rtseki/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.rtseki.course.entities.User;
 import com.rtseki.course.repositories.UserRepository;
+import com.rtseki.course.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
